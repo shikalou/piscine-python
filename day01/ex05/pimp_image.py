a09343fd954ea0deb57b488e4ee82e0696bea38b
Luna DINAUT <ldinaut@bess-f1r3s5.clusters.42paris.fr>
2024-02-07 20:04:14 +0100

    finish day01/ex05

--- day01/ex05/pimp_image.py
+++ day01/ex05/pimp_image.py
@@ -4,36 +4,44 @@ import cv2 as cv
 
 
 def ft_invert(array) -> any:
-    """
-    Inverts the color of the image received.
-    """
-    print("lol")
+    """Inverts the color of the image received."""
+    new_array = 255 - array
+    plt.imshow(new_array)
+    plt.show()
+    return (new_array)
 
 
 def ft_red(array) -> any:
-    """
-    Apply red filter to the image received.
-    """
-    print("lol")
+    """Apply red filter to the image received."""
+    im_R = array.copy()
+    im_R[:, :, (1, 2)] = 0
+    plt.imshow(im_R)
+    plt.show()
 
 
 def ft_green(array) -> any:
-    """
-    Apply green filter to the image received.
-    """
-    print("lol")
+    """Apply green filter to the image received."""
+    im_G = array.copy()
+    im_G[:, :, (0, 2)] = 0
+    plt.imshow(im_G)
+    plt.show()
 
 
 def ft_blue(array) -> any:
-    """
-    Apply blue filter to the image received.
-    """
-    print("lol")
+    """Apply blue filter to the image received."""
+    im_B = array.copy()
+    im_B[:, :, (0, 1)] = 0
+    plt.imshow(im_B)
+    plt.show()
 
 
 def ft_grey(array) -> any:
-    """
-    Apply grey filter to the image received.
-    """
-    print("lol")
+    """Apply grey filter to the image received."""
+    gray = array.copy()
+    r, g, b = array[:,:,0], array[:,:,1], array[:,:,2]
+    # gray = 0.3 * r + 0.59 * g + 0.11 * b
+    # gray = sum([r / 0.3 / 25, g / 0.59 / 25, b / 0.11 / 25])
+    gray = r / 0.299 / 25 / (0.587/ 25 / g) / (0.114/ 25 / b )
+    plt.imshow(gray, cmap="gray")
+    plt.show()
 
--- day01/ex05/tester.py
+++ day01/ex05/tester.py
@@ -1,9 +1,6 @@
 from load_image import ft_load
-from pimp_image import ft_invert
-from pimp_image import ft_red
-from pimp_image import ft_green
-from pimp_image import ft_blue
-from pimp_image import ft_grey
+from pimp_image import ft_invert, ft_red, ft_green, ft_blue, ft_grey
+import matplotlib.pyplot as plt
 
 
 def main():
@@ -11,10 +8,13 @@ def main():
     main function calls several function to modify an img
     """
     array = ft_load("landscape.jpg")
-    ft_invert(array)
-    ft_red(array)
-    ft_green(array)
-    ft_blue(array)
+    print(array)
+    # plt.imshow(array)
+    # plt.show()
+    # ft_invert(array)
+    # ft_red(array)
+    # ft_green(array)
+    # ft_blue(array)
     ft_grey(array)
     print(ft_invert.__doc__)
 
